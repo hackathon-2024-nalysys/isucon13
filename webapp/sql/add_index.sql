4a8384ced51ffccd2a4ed378e00aca5ac71e05d3
kota-makino-lvgs <kota.makino@leverages.jp>
2024-11-24 11:55:41 +0900

    ライブコメントのスパム削除処理を改善し、インデックス追加用のSQLスクリプトを作成

---
+++ webapp/sql/add_index.sql
@@ -0,0 +1,5 @@
+ALTER TABLE livestream_tags ADD INDEX (tag_id);
+ALTER TABLE livestream_tags ADD INDEX (livestream_id);
+-- ALTER TABLE isudns.records ADD INDEX (domain_id,name);
+ALTER TABLE icons ADD INDEX (user_id);
+ALTER TABLE isudns.records ADD INDEX (name);
